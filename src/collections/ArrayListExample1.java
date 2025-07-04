417d56fc3bb0c490f61e22bf610a1695e5e79432
Smyrnov Vlad <vladsimovv@gmail.com>
2025-07-04 15:56:51 +0300

    Collections Practice01

---
+++ src/collections/ArrayListExample1.java
@@ -0,0 +1,25 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class ArrayListExample1 {
+    public static void main(String[] args) {
+        ArrayList<String> list1 = new ArrayList<String>();
+        list1.add("Vlad");
+        list1.add("Oleg");
+        list1.add("Mike");
+
+        System.out.println(list1);
+
+
+
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("?");
+        list2.add("!");
+        list2.add(")");
+
+        list1.addAll(list2);
+
+        System.out.println(list1);
+    }
+}
